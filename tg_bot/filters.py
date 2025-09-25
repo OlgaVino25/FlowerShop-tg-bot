2b36f53ebf30a530cfae7481f560498096c89127
OlgaVino25 <olgavinokyrova25@yandex.ru>
2025-09-25 16:02:11 +0300

    Create filters.py

---
+++ tg_bot/filters.py
@@ -0,0 +1,53 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from demo_data.demo_db import get_occasions, get_color_schemes, get_flowers
+import demo_data.demo_db as db
+
+def occasion_filter(message):
+    occasions = [occ.title for occ in get_occasions()]
+    return message.text in occasions or message.text == "другой повод"
+
+def budget_filter(message):
+    budgets = ["~500", "~1000", "~2000", "больше", "не важно"]
+    return message.text in budgets
+
+def color_scheme_filter(message):
+    schemes = [scheme.title for scheme in get_color_schemes()]
+    schemes.append("не важно")
+    return message.text in schemes
+
+def flower_exclusion_filter(message):
+    flowers = [flower.title for flower in get_flowers()]
+    flower_buttons = [f"❌ {flower}" for flower in flowers] + [f"✅ {flower}" for flower in flowers]
+    flower_buttons.append("✅ Завершить выбор")
+    return message.text in flower_buttons
+
+def contact_filter(message):
+    return message.content_type == "contact"
+
+# Фильтры для заказов
+def address_filter(message, user_data):
+    user_id = message.chat.id
+    return (user_id in user_data and 
+            hasattr(user_data[user_id], "phone") and 
+            not hasattr(user_data[user_id], "order_address"))
+
+def date_filter(message, user_data):
+    user_id = message.chat.id
+    return (user_id in user_data and 
+            hasattr(user_data[user_id], "order_address") and 
+            not hasattr(user_data[user_id], "delivery_date"))
+
+def time_filter(message, user_data):
+    user_id = message.chat.id
+    return (user_id in user_data and 
+            hasattr(user_data[user_id], "delivery_date") and 
+            not hasattr(user_data[user_id], "delivery_time"))
+
+def comment_filter(message, user_data):
+    user_id = message.chat.id
+    return (user_id in user_data and 
+            hasattr(user_data[user_id], "delivery_time") and 
+            not hasattr(user_data[user_id], "comment"))
